4a68f98f800acef89f4ecf1d1d1de4ea40f2a893
jasonserviss <jason.serviss@ki.se>
2017-10-26 20:05:41 +0200

    rewrote all the data-raw code to use tidyverse

--- data-raw/saveRDA.R
+++ data-raw/saveRDA.R
@@ -1,36 +1,37 @@
+library(tidyverse)
+
 #HCTp53null
-data <- read.table('./data-raw/HCTp53null.txt', sep = "\t", header = TRUE)
-colnames(data)[which(colnames(data) == "Target")] <- "Gene"
-save(data, file = './data/HCT116null.rda', compress = "bzip2")
+read_tsv('./data-raw/HCTp53null.txt', col_names = TRUE) %>%
+    rename(gene = Target) %>%
+    write_rds(., path = './data/HCT116p53null.rds', compress = "bz2")
 
 #HctHekDox
-data <- read.table('./data-raw/HctHekDox.txt', sep = "\t", header = TRUE)
-save(data, file = './data/HctHekDox.rda', compress = "bzip2")
+read_tsv('./data-raw/HctHekDox.txt', col_names = TRUE) %>%
+    write_rds(., path = './data/HctHekDox.rds', compress = "bz2")
 
 #P1-HCTandHEK
-data <- read.table('./data-raw/P1-HCTandHEK.txt', sep = "\t", header = TRUE)
-save(data, file = './data/P1-HCTandHEK.rda', compress = "bzip2")
+read_tsv('./data-raw/P1-HCTandHEK.txt', col_names = TRUE) %>%
+    write_rds(., path = './data/P1-HCTandHEK.rds', compress = "bz2")
 
 #p1shRNAdox
-data <- read.table('./data-raw/p1shRNAdox.txt', sep = "\t", header = TRUE)
-save(data, file = './data/p1shRNAdox.rda', compress = "bzip2")
+read_tsv('./data-raw/p1shRNAdox.txt', col_names = TRUE) %>%
+    write_rds(., path = './data/p1shRNAdox.rds', compress = "bz2")
 
 #stableLineExpression
-data <- read.table('./data-raw/stableLineExpression.txt', sep = "\t", header = TRUE)
-save(data, file = './data/stableLineExpression.rda', compress = "bzip2")
+read_tsv('./data-raw/stableLineExpression.txt', col_names = TRUE) %>%
+    rename(gene = Gene) %>%
+    write_rds(., path = './data/stableLineExpression.rds', compress = "bz2")
 
 #stableLineCellCycle
-data <- read.table('./data-raw/stableLineCellCycle.txt', sep = "\t", header = TRUE)
-save(data, file = './data/stableLineCellCycle.rda', compress = "bzip2")
+read_tsv('./data-raw/stableLineCellCycle.txt', col_names = TRUE) %>%
+    write_rds(., path = './data/stableLineCellCycle.rds', compress = "bz2")
 
 #growthStarvation
-data <- read.table('./data-raw/growthStarvation.txt', sep = "\t", header = TRUE)
-save(data, file = './data/growthStarvation.rda', compress = "bzip2")
+read_tsv('./data-raw/growthStarvation.txt', col_names = TRUE) %>%
+    write_rds(., path = './data/growthStarvation.rds', compress = "bz2")
 
 #stableLinePolIIChIP
-data <- read.table('./data-raw/stableLinePolIIChIP.txt', sep = "\t", header = TRUE)
-save(data, file = './data/stableLinePolIIChIP.rda', compress = "bzip2")
-
-
+read_tsv('./data-raw/stableLinePolIIChIP.txt', col_names = TRUE) %>%
+    write_rds(., path = './data/stableLinePolIIChIP.rds', compress = "bz2")
 
 #source('./data-raw/saveRDA.R')
