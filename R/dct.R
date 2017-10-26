3d75cd8a11f18c50c26e2302e4abfd21fe8c178d
jasonserviss <jason.serviss@ki.se>
2017-10-26 19:46:27 +0200

    modifying required gene colname in dct function

--- R/dct.R
+++ R/dct.R
@@ -1,10 +1,10 @@
 
 dct <- function(data, goi, hk) {
-    #genes should be in "Gene" column
-    g <- dplyr::filter(data, Gene %in% goi) %>%
+    #genes should be in "gene" column
+    g <- dplyr::filter(data, gene %in% goi) %>%
         dplyr::rename(Ct1GOI = Ct1, Ct2GOI = Ct2)
         
-    h <- dplyr::filter(data, Gene %in% hk)  %>%
+    h <- dplyr::filter(data, gene %in% hk)  %>%
         dplyr::rename(Ct1HK = Ct1, Ct2HK = Ct2)
     
     cbind(g, dplyr::select(h, Ct1HK, Ct2HK)) %>%
