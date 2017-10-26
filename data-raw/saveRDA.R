b6c8d9e75592da98f1c43c8b0310fa0ae32af458
jasonserviss <jason.serviss@ki.se>
2017-10-26 18:56:29 +0200

    further work on vignette, fixing HCT116null.rda

--- data-raw/saveRDA.R
+++ data-raw/saveRDA.R
@@ -1,5 +1,6 @@
 #HCTp53null
 data <- read.table('./data-raw/HCTp53null.txt', sep = "\t", header = TRUE)
+colnames(data)[which(colnames(data) == "Target")] <- "Gene"
 save(data, file = './data/HCT116null.rda', compress = "bzip2")
 
 
