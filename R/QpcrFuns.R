8c2155d0467bb36c3c65019d9f26bdb1755aa9e9
jasonserviss <jason.serviss@ki.se>
2017-10-27 18:15:51 +0200

    work on vignette and QpcrFuns

--- R/QpcrFuns.R
+++ R/QpcrFuns.R
@@ -12,7 +12,81 @@ dct <- function(data) {
     mutate(dct = meanGOI - meanHK)
 }
 
-ddct <- function(data, ) {
-    group_by(Treatment, Biological.replicate, Technical.replicate, Time, cellLine) %>%
-    mutate(normConfluency = Confluency / pull(data, Confluency)[data$Treatment == Treatment & data$Biological.replicate == Biological.replicate & data$Technical.replicate == Technical.replicate & data$Time == 0 & data$cellLine == cellLine])
+#grouping variable should not include cnt.group
+ddct <- function(data, cnt.group, cnt.value, grouping) {
+    quo.cnt.group <- enquo(cnt.group)
+    cnt <- dplyr::filter(data, (!!quo.cnt.group) == cnt.value) %>%
+    select(grouping, dct)
+    
+    data %>%
+    left_join(cnt, by = grouping) %>%
+    mutate(ddct = dct.x - dct.y) %>%
+    select(-dct.y) %>%
+    rename(dct = dct.x)
 }
+
+folds <- function(data) {
+    data %>%
+    mutate(
+        fold = 2^-.data$ddct,
+        log2fold = log2(2^-.data$ddct)
+    )
+}
+
+calcStats <- function(data, cnt.group, cnt.value, grouping) {
+    quo.cnt.group <- enquo(cnt.group)
+    cnt <- dplyr::filter(data, (!!quo.cnt.group) == cnt.value) %>%
+    select(grouping, log2fold, cnt.group)
+    
+    
+    data %>%
+    select(grouping, log2fold, cnt.group) %>%
+    left_join(cnt, by = grouping) %>%
+    group_by_at(grouping[grouping != "Biological Replicate"]) %>%
+    summarize(
+        n = n(),
+        mean = mean(.data$log2fold.x),
+        CI95l = t.test(.data$log2fold.x)$conf.int[1],
+        CI95h = t.test(.data$log2fold.x)$conf.int[2],
+        pValue = t.test(.data$log2fold.x, .data$log2fold.y)$p.value
+    )
+}
+
+t.test(subset(data, gene == "miR34a AS" & Treatment == 100 & Condition == "WT", select = "log2fold")[[1]], subset(data, gene == "miR34a AS" & Treatment == 100 & Condition == "p53 null", select = "log2fold")[[1]])
+
+
+library(tidyverse)
+
+
+
+test <- tibble(name = LETTERS[1:10])
+y <- "A"
+
+quo.x <- quo(name)
+test %>% filter((!!quo.x) == y)
+
+testFun <- function(data, name, value) {
+    quo.name <- enquo(name)
+    filter(data, (!!quo.name) == value)
+}
+
+expected <- tibble(name = "A")
+output <- testFun(test, name, y)
+output
+testthat::expect_identical(expected, output)
+
+
+test1 <- tibble(var = 1:11)
+y1 <- 1L
+
+quo.x1 <- quo(var)
+test1 %>% filter((!!quo.x1) == y1)
+
+expected1 <- tibble(var = 1L)
+output1 <- testFun(test1, var, y1)
+output1
+testthat::expect_identical(expected1, output1)
+
+
+
+sessionInfo()
