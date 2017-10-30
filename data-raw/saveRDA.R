c79e1fc797ad1c972737458c71dfa00b7d368652
jasonserviss <jason.serviss@ki.se>
2017-10-30 16:41:11 +0100

    updating preprocessing p1shRNAdox again2

--- data-raw/saveRDA.R
+++ data-raw/saveRDA.R
@@ -61,7 +61,8 @@ read_tsv('./data-raw/p1shRNAdox.txt', col_names = TRUE) %>%
         shRNA = gsub("shRenilla", "shRNA Renilla", .data$shRNA),
         gene = gsub("Luciferase", "  Luciferase\n(miR34a HG)", .data$gene),
         gene = gsub("Renilla", "        Renilla\n(miR34a asRNA)", .data$gene),
-        gene = gsub("B-actin", "Actin", .data$gene)
+        gene = gsub("B-actin", "Actin", .data$gene),
+        alias = ifelse(is.na(.data$alias), "Actin", .data$alias)
     ) %>%
     save(., file = './data/p1shRNAdox.rda', compress = "bzip2")
 
