3e9c1eaab03719deca3047b73b41368c1ee52266
jasonserviss <jason.serviss@ki.se>
2017-11-03 17:52:40 +0100

    added med arg to ddct

--- R/QpcrFuns.R
+++ R/QpcrFuns.R
@@ -1,5 +1,8 @@
 #' @export
-technicalMeans <- function(data, grouping) {
+technicalMeans <- function(
+    data,
+    grouping
+){
     data %>%
     gather(`Tech. Replicate`, Ct, -grouping) %>%
     group_by(!!! rlang::syms(grouping)) %>%
@@ -8,7 +11,12 @@ technicalMeans <- function(data, grouping) {
 }
 
 #' @export
-dct <- function(data, GOIs, HK, grouping) {
+dct <- function(
+    data,
+    GOIs,
+    HK,
+    grouping
+){
     quo.hk <- enquo(HK)
     grouping1 <- grouping[grouping != "gene"]
     grouping2 <- c(grouping1[grouping1 != "Biological Replicate"], "GOI")
@@ -17,14 +25,25 @@ dct <- function(data, GOIs, HK, grouping) {
     spread(gene, ctMean) %>%
     mutate(HK = quo_name(quo.hk)) %>%
     rename(ctMeanHK = !!quo.hk) %>%
-    gather(GOI, ctMeanGOI, -one_of(names(.)[!names(.) %in% GOIs]), factor_key = TRUE) %>%
+    gather(
+        GOI,
+        ctMeanGOI,
+        -one_of(names(.)[!names(.) %in% GOIs]),
+        factor_key = TRUE
+    ) %>%
     group_by(!!!rlang::syms(grouping2)) %>%
     mutate(dct = ctMeanGOI - median(ctMeanHK)) %>%
     ungroup() %>%
     select(grouping1, HK, ctMeanHK, GOI, ctMeanGOI, dct)
 }
 
-ddct <- function(data, logical, grouping) {
+#' @export
+ddct <- function(
+    data,
+    logical,
+    grouping,
+    med = TRUE
+){
     grouping1 <- grouping[grouping != "Biological Replicate"]
     logical.grps <- colnames(logical)
     
@@ -40,14 +59,16 @@ ddct <- function(data, logical, grouping) {
     data %>%
     left_join(cnt, by = grouping[!grouping %in% logical.grps]) %>%
     group_by(!!!rlang::syms(grouping1)) %>%
-    mutate(ddct = dct.x - dct.y) %>%
+    mutate(ddct = dct.x - case_when(med ~ median(dct.y), TRUE ~ dct.y)) %>%
     ungroup() %>%
     select(-dct.y) %>%
     rename(dct = dct.x)
 }
 
 #' @export
-folds <- function(data) {
+folds <- function(
+    data
+){
     data %>%
     mutate(
         fold = 2^-.data$ddct,
@@ -56,14 +77,23 @@ folds <- function(data) {
 }
 
 #' @export
-calcStats <- function(data, cnt.group, cnt.value, grouping) {
+calcStats <- function(
+    data,
+    cnt.group,
+    cnt.value,
+    grouping
+){
     quo.cnt.group <- enquo(cnt.group)
     cnt <- filter(data, (!!quo.cnt.group) == cnt.value) %>%
     select(grouping, log2fold, !!quo.cnt.group)
     
     data %>%
     select(grouping, log2fold, !!quo.cnt.group) %>%
-    full_join(cnt, by = grouping[grouping != quo_name(quo.cnt.group)], suffix = c("", ".y")) %>%
+    full_join(
+        cnt,
+        by = grouping[grouping != quo_name(quo.cnt.group)],
+        suffix = c("", ".y")
+    ) %>%
     group_by_at(grouping[grouping != "Biological Replicate"]) %>%
     summarize(
         n = n(),
@@ -72,37 +102,13 @@ calcStats <- function(data, cnt.group, cnt.value, grouping) {
         CI95h = t.test(.data$log2fold)$conf.int[2],
         pValue = t.test(.data$log2fold, .data$log2fold.y)$p.value
     ) %>%
-    #filter((!!quo.cnt.group) != cnt.value) %>%
     ungroup()
 }
 
-#t.test(subset(data, gene == "miR34a AS" & Treatment == 100 & Condition == "WT", select = "log2fold")[[1]], subset(data, gene == "miR34a AS" & Treatment == 100 & Condition == "p53 null", select = "log2fold")[[1]])
-
-#library(tidyverse)
-#test <- tibble(name = LETTERS[1:10])
-#y <- "A"
-#quo.x <- quo(name)
-#test %>% filter((!!quo.x) == y)
-#testFun <- function(data, name, value) {
-#    quo.name <- enquo(name)
-#    filter(data, (!!quo.name) == value)
-#}
-#expected <- tibble(name = "A")
-#output <- testFun(test, name, y)
-#output
-#testthat::expect_identical(expected, output)
-#test1 <- tibble(var = 1:11)
-#y1 <- 1L
-#quo.x1 <- quo(var)
-#test1 %>% filter((!!quo.x1) == y1)
-#expected1 <- tibble(var = 1L)
-#output1 <- testFun(test1, var, y1)
-#output1
-#testthat::expect_identical(expected1, output1)
-#sessionInfo()
-
 #' @export
-pFormat <- function(stats) {
+pFormat <- function(
+    stats
+){
     idx <- which(colnames(stats) == "pValue")
     mutate(stats, pFormat = case_when(
         is.nan(pValue) | pValue == 1 ~ "",
