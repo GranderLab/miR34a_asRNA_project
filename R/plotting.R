af71192881f843f1405c745d21fa86c70ac90bd5
jasonserviss <jason.serviss@ki.se>
2017-11-07 18:14:25 +0100

    added pdf and markdown styles

--- R/plotting.R
+++ R/plotting.R
@@ -1,3 +1,67 @@
 
 #add "geoms" for rmarkdown and pdf
 
+plotPDF <- function() {
+  style <- theme_few() +
+    scale_fill_ptol() +
+    theme(
+        plot.title = element_text(
+            hjust = 0.5,
+            face = "bold",
+            size = 10,
+            margin = margin(b = 2, unit = "mm")
+        ),
+        plot.margin = unit(rep(0.1, 4), "lines"),
+        legend.position = "top",
+        legend.margin = unit(1, "mm"),
+        legend.key.size = unit(1/5, "cm"),
+        legend.title = element_text(size = 9),
+        legend.text = element_text(size = 7),
+        axis.title = element_text(size = 9),
+        axis.title.x = element_text(margin = margin(t = 5)),
+        axis.text.y = element_text(size = 8),
+        axis.text.x = element_text(size = 7),
+        axis.ticks = element_line(size = 0.25),
+        axis.ticks.length = unit(1/15, "cm"),
+        strip.text.x = element_text(size = 8, margin = margin(0.01, 0, 0.1, 0, "cm")),
+        panel.border = element_rect(fill = NA, size = 0.15)
+    ) +
+    guides(
+        fill = guide_legend(override.aes = list(size = 2)),
+        colour = FALSE
+    )
+    
+    return(style)
+}
+
+plotRmarkdown <- function() {
+  style <- theme_few() +
+  scale_colour_ptol() +
+  scale_fill_ptol() +
+  theme(
+    legend.position = "top",
+    legend.title = element_text(size = 17),
+    legend.text = element_text(size = 15),
+    plot.title = element_text(
+      hjust = 0.5,
+      face = "bold",
+      size = 20
+    ),
+    plot.caption = element_text(
+      hjust = 0,
+      margin = margin(t = 15),
+      family = "Arial Unicode MS"
+    ),
+    axis.title = element_text(size = 17),
+    axis.title.x = element_text(margin = margin(t = 10)),
+    axis.text = element_text(size = 12),
+    strip.text.x = element_text(size = 17)
+  ) +
+  guides(
+    fill = guide_legend(title = "Gene", override.aes = list(size = 4)),
+    colour = FALSE
+  )
+  
+  return(style)
+}
+
