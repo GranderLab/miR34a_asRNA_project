b54fa05bdf0e8000579fdcf424499313f79d4ed7
jasonserviss <jason.serviss@ki.se>
2017-11-08 14:28:42 +0100

    removed guides from plotting add-ons as they should be plot specific

--- R/plotting.R
+++ R/plotting.R
@@ -1,8 +1,8 @@
 
-#add "geoms" for rmarkdown and pdf
-
-plotPDF <- function() {
-  style <- theme_few() +
+#' @export
+plotPDF <- function(plot) {
+  p <- plot +
+    theme_few() +
     scale_fill_ptol() +
     theme(
         plot.title = element_text(
@@ -25,43 +25,37 @@ plotPDF <- function() {
         axis.ticks.length = unit(1/15, "cm"),
         strip.text.x = element_text(size = 8, margin = margin(0.01, 0, 0.1, 0, "cm")),
         panel.border = element_rect(fill = NA, size = 0.15)
-    ) +
-    guides(
-        fill = guide_legend(override.aes = list(size = 2)),
-        colour = FALSE
     )
     
-    return(style)
+    return(p)
 }
 
-plotRmarkdown <- function() {
-  style <- theme_few() +
-  scale_colour_ptol() +
-  scale_fill_ptol() +
-  theme(
-    legend.position = "top",
-    legend.title = element_text(size = 17),
-    legend.text = element_text(size = 15),
-    plot.title = element_text(
-      hjust = 0.5,
-      face = "bold",
-      size = 20
-    ),
-    plot.caption = element_text(
-      hjust = 0,
-      margin = margin(t = 15),
-      family = "Arial Unicode MS"
-    ),
-    axis.title = element_text(size = 17),
-    axis.title.x = element_text(margin = margin(t = 10)),
-    axis.text = element_text(size = 12),
-    strip.text.x = element_text(size = 17)
-  ) +
-  guides(
-    fill = guide_legend(title = "Gene", override.aes = list(size = 4)),
-    colour = FALSE
-  )
+#' @export
+plotRmarkdown <- function(plot) {
+  p <-  plot +
+    theme_few() +
+    scale_colour_ptol() +
+    scale_fill_ptol() +
+    theme(
+      legend.position = "top",
+      legend.title = element_text(size = 17),
+      legend.text = element_text(size = 15),
+      plot.title = element_text(
+        hjust = 0.5,
+        face = "bold",
+        size = 20
+      ),
+      plot.caption = element_text(
+        hjust = 0,
+        margin = margin(t = 15),
+        family = "Arial Unicode MS"
+      ),
+      axis.title = element_text(size = 17),
+      axis.title.x = element_text(margin = margin(t = 10)),
+      axis.text = element_text(size = 12),
+      strip.text.x = element_text(size = 17)
+    )
   
-  return(style)
+  return(p)
 }
 
