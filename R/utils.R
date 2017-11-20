79a3d21aae3554a522de21b6c87257431d8dcc9e
jasonserviss <jason.serviss@ki.se>
2017-11-20 13:57:05 +0100

    re-running Rmds after addition of suppFig3

--- R/utils.R
+++ R/utils.R
@@ -29,9 +29,8 @@ fileMap <- function(type) {
     figure1f = "coding_potential_cpat/coding_potential_cpat.Rmd",
     figure2a = "hct116_p53_null/hct116_p53_null.Rmd",
     figure2b = "hct116_hek293t_dox/hct116_hek293t_dox.Rmd",
-    figure2c = "p1_construct/p1_construct.Rmd",
-    figure2d = "p1_hct116_hek293t/p1_hct116_hek293t.Rmd",
-    figure2e = "p1_shrna_renilla_dox/p1_shrna_renilla_dox.Rmd",
+    figure2c = "p1_hct116_hek293t/p1_hct116_hek293t.Rmd",
+    figure2d = "p1_shrna_renilla_dox/p1_shrna_renilla_dox.Rmd",
     figure3a = "stable_line_expression/stable_line_expression.Rmd",
     figure3b = "stable_line_cell_cycle/stable_line_cell_cycle.Rmd",
     figure3c = "stable_line_growth_starvation/stable_line_growth_starvation.Rmd",
@@ -44,11 +43,13 @@ fileMap <- function(type) {
     suppFigure1e = "cellular_localization/cellular_localization.Rmd",
     suppFigure1f = "transcript_stability/transcript_stability.Rmd",
     suppFigure1g = "coding_potential_cpc/coding_potential_cpc.Rmd",
-    suppFigure2a = "stable_line_expression_hek293t/stable_line_expression_hek293t.Rmd",
-    suppFigure2b = "stable_line_ccnd1_exp/stable_line_ccnd1_exp.Rmd",
-    suppFigure2c = "stable_line_ccnd1_prot/stable_line_ccnd1_prot.Rmd",
-    suppFigure2d = "lnc34a_cage/lnc34a_cage.Rmd",
-    suppFigure2e = "lnc34a_splice_jnc/lnc34a_splice_jnc.Rmd"
+    suppFigure2a = "p1_construct/p1_construct.Rmd",
+    suppFigure2b = "p1_hek293t/p1_hek293t.Rmd",
+    suppFigure3a = "stable_line_expression_hek293t/stable_line_expression_hek293t.Rmd",
+    suppFigure3b = "stable_line_ccnd1_exp/stable_line_ccnd1_exp.Rmd",
+    suppFigure3c = "stable_line_ccnd1_prot/stable_line_ccnd1_prot.Rmd",
+    suppFigure3d = "lnc34a_cage/lnc34a_cage.Rmd",
+    suppFigure3e = "lnc34a_splice_jnc/lnc34a_splice_jnc.Rmd"
   )
   
   if(type == "Rmd") {
